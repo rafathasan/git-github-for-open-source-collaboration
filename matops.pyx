06462f485c456a127ad46e9af49456b9b13d21ae
rafathasan <rafathasankhan@gmail.com>
2020-09-05 04:10:24 +0600

    created new file

---
+++ matops.pyx
@@ -0,0 +1,10 @@
+try:
+    cimport libc.stdlib as lib
+except:
+    print('module not found')
+
+def list square(list mat):
+    for i,row in enumerate(mat):
+        for j,val in enumerate(row[i]):
+            row[j] = row[j]*row[j]
+    return mat
